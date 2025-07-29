84ea53fa6a8f69317c64b43518601a1cf25dab97
Sravani Dash <sravanipoornimadash@gmail.com>
2025-07-29 09:48:00 -0400

    step 2 - data preprocessing

--- classifier1.py
+++ classifier1.py
@@ -1,7 +1,19 @@
 import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
 
-# Step 1 - Explore the data
+# Step 1 - Load data
+
 books = pd.read_csv('goodreads_books.csv', on_bad_lines='skip')
-print(books.head())
-print(books.info())
-print(books.isnull().sum())
+
+# Step 2 - Data Preprocessing
+
+# Remove spaces in cols
+new_cols = [col.strip() for col in books.columns] 
+books.columns = new_cols
+
+# Combine authors and title into 1 "content" field
+books['content'] = books['authors'].str.lower() + ' ' + books['title'].str.lower() 
+
+# Step 3 - Build Recommender
+
